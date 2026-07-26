31ca839b501d28d5d0a42e466d6df8540e63994a
MarianMoawed <marianmoawed9@gmail.com>
2026-07-26 13:19:33 +0000

    is_data_exists function created

--- src/ingestion/kaggle_extractor.py
+++ src/ingestion/kaggle_extractor.py
@@ -21,22 +21,37 @@ class KaggleExtractor:
             shutil.rmtree(self.current_data_dir)
         os.makedirs(self.current_data_dir, exist_ok=True)
 
+    def is_data_exists(self) -> bool:
+        
+        if os.path.exists(self.current_data_dir) and os.listdir(self.current_data_dir):
+            files = [os.path.join(self.current_data_dir, f) for f in os.listdir(self.current_data_dir)]
+            if any(os.path.getsize(f) > 0 for f in files):
+                return True
+        return False
+
 
     def download_dataset(self, dataset_name):
+        
+        if self.is_data_exists():
+            return True
+            
         try:
             self.clean_data_directory()
             logger.info(response.DOWNLOAD_DATASET_START.value.format(dataset_name=dataset_name))
 
             self.api.dataset_download_files(
-            dataset_name,
-            path=self.current_data_dir, unzip=True
+                dataset_name,
+                path=self.current_data_dir, 
+                unzip=True
             )
-            if os.path.exists(self.current_data_dir)and os.listdir(self.current_data_dir):
-                files= [os.path.join(self.current_data_dir, f) for f in os.listdir(self.current_data_dir) ]
-
-                if any (os.path.getsize(f) > 0 for f in files):
-                    logger.info(response.DOWNLOAD_DATASET_SUCCESS.value.format(dataset_name=dataset_name, download_path=self.current_data_dir))
-                    return True
+            
+             
+            if self.is_data_exists():
+                logger.info(response.DOWNLOAD_DATASET_SUCCESS.value.format(
+                    dataset_name=dataset_name, 
+                    download_path=self.current_data_dir
+                ))
+                return True
                 
             raise FileNotFoundError(response.DOWNLOAD_DATASET_FAILED.value.format(dataset_name=dataset_name, error="downloaded files are missing or empty."))
     
